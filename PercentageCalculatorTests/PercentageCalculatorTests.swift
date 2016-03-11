c055db2b284021e1ce9cf07eef2be0257df2c3f3
rocooshiang <rocoo.shaing@gmail.com>
2016-03-11 15:50:35 +0800

    Pracetice swift unit test.

--- PercentageCalculatorTests/PercentageCalculatorTests.swift
+++ PercentageCalculatorTests/PercentageCalculatorTests.swift
@@ -10,27 +10,35 @@ import XCTest
 @testable import PercentageCalculator
 
 class PercentageCalculatorTests: XCTestCase {
+  var vc: ViewController!
+  
+  override func setUp() {
+    super.setUp()
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    let storyboard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+    vc = storyboard.instantiateInitialViewController() as! ViewController
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+  }
+  
+  func testPercentageCalculator() {
+    // Should be 25
+    let p = vc.percentage(50, 50)
+    XCTAssert(p == 25)
+  }
+  
+  func testLabelValuesShowedProperly() {
+    //Initialize view
+    let _ = vc.view
+    vc.updateLabels(Float(80.0), Float(50.0), Float(40.0))
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+    // The labels should now display 80, 50 and 40
+    XCTAssert(vc.numberLabel.text == "80.0", "numberLabel doesn't show the right text")
+    XCTAssert(vc.percentageLabel.text == "50.0%", "percentageLabel doesn't show the right text")
+    XCTAssert(vc.resultLabel.text == "40.0", "resultLabel doesn't show the right text")
+  }
+  
 }
